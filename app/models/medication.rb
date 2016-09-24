9496d8e941c028313f841379cf0d919f63d8ec6c
Millie Chan <mireechan@gmail.com>
2016-09-24 15:59:56 -0700

    Deleted strength model,migration

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,2 +1,3 @@
 class Medication < ApplicationRecord
+  has_many :users
 end
--- db/migrate/20160924225528_create_strengths.rb
+++
@@ -1,9 +0,0 @@
-class CreateStrengths < ActiveRecord::Migration[5.0]
-  def change
-    create_table :strengths do |t|
-      t.text :dosage
-
-      t.timestamps
-    end
-  end
-end
