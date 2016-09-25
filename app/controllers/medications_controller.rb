094cc9dc83c0991e8612a522270e096c482b2e16
Millie Chan <mireechan@gmail.com>
2016-09-24 19:23:51 -0700

    Render pages for controller  actions new, create, index

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,12 +1,18 @@
 class MedicationsController < ApplicationController
   def new
     @medication = Medication.new 
+    render :new
   end
 
   def create 
+    @medication = Medication.create(name: params[:name])
+    render :new
   end
 
   def index
+    user = User.find(1)
+    @medications = user.medications
+    render :index
   end
 
   def edit
