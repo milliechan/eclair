c049b46502f2ae95dac8a5205b55d9758494f466
Millie Chan <mireechan@gmail.com>
2016-09-24 19:10:38 -0700

    Add all controller actions and fill out new method

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,20 @@
 class MedicationsController < ApplicationController
+  def new
+    @medication = Medication.new 
+  end
+
+  def create 
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update 
+  end
+
+  def destroy 
+  end 
 end
