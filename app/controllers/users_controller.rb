0ae2f11bd9aa59daeb89932d0928b621ef138bb7
armoney <armonarcuri@yahoo.com>
2016-09-25 01:27:59 -0700

    Add donut route, add donut helper module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
-
+	include UsersHelper
   def show
     @user = User.find(params[:id])
     @medications = @user.medications
-    
+
+    graph_output
+
     render :show
   end
+
+  def donut
+		render json: { graph: graph_output }, status: 200
+	end
 
 end
