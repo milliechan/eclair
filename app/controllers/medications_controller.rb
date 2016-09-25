81d372988a0ed495e9483167df37f46cc5bf6ae8
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-09-25 02:46:49 -0700

    user can add new meds

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,12 +1,15 @@
 class MedicationsController < ApplicationController
   def new
     @medication = Medication.new 
-    render :new
+    render :index
   end
 
   def create 
+    @user = User.find(1)
     @medication = Medication.create(name: params[:name])
-    render :new
+    UserMedication.create(user_id: 1, medication_id: @medication.id)
+    @medications = @user.medications
+    render :index
   end
 
   def index
