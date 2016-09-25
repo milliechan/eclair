c522b2f4d71f91b760402fd8373fecd2b956fcf7
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-09-24 20:52:31 -0700

    Update index, show and application html and js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -104,35 +104,35 @@ $(document).ready(function() {
     });
   }
 
-  // initializeCostEstimateGraph();
+  initializeCostEstimateGraph();
 
-  function loadGraphWhenInView() {
-    // init controller
-	var controller = new ScrollMagic.Controller();
-
-	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: ".features", duration: 200, offset: 0})
-					.addTo(controller)
-          .setPin("#graph")
-					// .addIndicators() // add indicators (requires plugin)
-					.on("update", function (e) {
-						$("#scrollDirection").text(e.target.controller().info("scrollDirection"));
-					})
-					.on("enter leave", function (e) {
-            if (e.type == "enter") {
-              initializeCostEstimateGraph();
-            }
-						$("#state").text(e.type == "enter" ? "inside" : "outside");
-					})
-					.on("start end", function (e) {
-						$("#lastHit").text(e.type == "start" ? "top" : "bottom");
-					})
-					.on("progress", function (e) {
-						$("#progress").text(e.progress.toFixed(3));
-					});
-  }
-
-  loadGraphWhenInView();
+  // function loadGraphWhenInView() {
+  //   // init controller
+	// var controller = new ScrollMagic.Controller();
+  //
+	// // build scene
+	// var scene = new ScrollMagic.Scene({triggerElement: ".features", duration: 200, offset: 0})
+	// 				.addTo(controller)
+  //         .setPin("#graph")
+	// 				// .addIndicators() // add indicators (requires plugin)
+	// 				.on("update", function (e) {
+	// 					$("#scrollDirection").text(e.target.controller().info("scrollDirection"));
+	// 				})
+	// 				.on("enter leave", function (e) {
+  //           if (e.type == "enter") {
+  //             initializeCostEstimateGraph();
+  //           }
+	// 					$("#state").text(e.type == "enter" ? "inside" : "outside");
+	// 				})
+	// 				.on("start end", function (e) {
+	// 					$("#lastHit").text(e.type == "start" ? "top" : "bottom");
+	// 				})
+	// 				.on("progress", function (e) {
+	// 					$("#progress").text(e.progress.toFixed(3));
+	// 				});
+  // }
+  //
+  // loadGraphWhenInView();
 
 
 });
